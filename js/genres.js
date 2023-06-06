30c6ed83d990b9b30fba5830c9a4e182fcf04f57
shawnzir <szir@udesa.edu.ar>
2023-06-05 22:53:08 -0300

    bastantes mejoras, agregue flex para el genres.html, le cambie el font a la pagina y mejore un toque el js

--- js/genres.js
+++ js/genres.js
@@ -1,17 +1,19 @@
 window.onload = function() {
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre")
-    .then(function(respuesta){
-        return respuesta.json();
+   let urlGeneros = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre" // Esta ruta se conoce como endpoint. De aqui sacaremos toda la informacion que necesitemos.
+   
+    fetch(urlGeneros) // Usamos fetch ya que nos permite consultar un recurso de forma asincronica, es decir, iniciar una accion sin depender de la finalizacion de acciones anteriores.
+    .then(function(respuesta){ // En este caso el parametro "(response)" es el que recibe la informacion del endpoint, obtenida por el fetch.
+        return respuesta.json(); // Utiilizamos el metodo "json()" con el fin de decodificar la informacion obtenida por el fetch y poder utilizarla en forma de arrays, objetos, etc... Al ser su proceso asincronico, necesitamos de otra promesa, es decir, un segundo "then"
     })
-    .then(function(informacion) {
+    .then(function(informacion) { // El parametro "informacion", recibe la informacion decodificada gracias al primer then. Dentro de esta funcion escribiremos el codigo para poder trabajar con la informacion obtenida del API.
         console.log(informacion.data)
 
         for(let i = 1; i < informacion.data.length; i++){
 
-            let gif = "<p>" + informacion.data[i].name + "</p>";
+            let gif = "<h3>" + informacion.data[i].name + "</h3>";
             gif += "<img src=" + informacion.data[i].picture + ">"
 
-            document.querySelector("#apisaqui").innerHTML += "<li>" + gif + "</li>"
+            document.querySelector(".content").innerHTML += "<article>" + gif + "</article>"
         }
     })
 }
---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Agregar endpoint de deezer para el buscador 
+window.onload = function() {
+    let queryString = new URLSearchParams(location.search); //Esto significa todo el querystring, es decir: buscar="cualquier cosa que hayamos buscado"
+    queryString.get("buscar")
+
+    console.log(queryString);
+}
