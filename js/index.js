94e94ae7aa0e90ff86764ad9bfde8d4dc133dc62
BautistaLignini <jlignini@udesa.edu.ar>
2023-06-08 15:43:27 -0300

    hice el buscador y estiy haceiendo el home

--- js/index.js
+++ js/index.js
@@ -24,3 +24,25 @@ Letrita.addEventListener('mouseout', function(e) {
 });
 
 console.log()
+
+
+
+// Buscador que ande 
+
+let form = document.querySelector('form')
+let inputdebusqueda= document.querySelector('input')
+
+form.addEventListener('submit', function(e){
+e.preventDefault()
+
+if(inputdebusqueda.value.length < 2){
+
+    alert("El campo tiene menos de 3 carateres, porfavor ingrese de vuelta su busqueda")
+    
+
+
+} else {
+    this.submit()
+}
+})
+
---
+++ js/search-results.js
@@ -0,0 +1,27 @@
+// El lugar donde buscar 
+
+//let qs = location.search //quarristrin en formato textp
+//let qstoobjects = new URLSearchParams(qs)
+
+//let datoabuscar =   qstoobjects.get("buscar") //necesito lo que el usuario escribo en el imput
+
+//let endpointbusqueda = "https://api.deezer.com/search?q=${datoabuscar}"
+
+//fetch(endpointbusqueda)
+//.then(function (response) {
+ //   return response.json()
+//})
+//.then(function (datos) {
+   // console.log(datos)
+
+    // mostrar resultados de busqueda en la pantalla
+
+    // mostar un aviso en caso de que la busqueda no tenga resultadso 
+//})
+//.catch(function (e) {
+ //   console.log(e)
+//})
+
+
+
+// si el array viene basyo podess con inner tex y if poner esto no tiene resultados
