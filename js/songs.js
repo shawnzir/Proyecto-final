ce7fb59799887b7b935634f1c8f79d764bff29e0
Mateo Poggio <mateopoggio@MacBook-Pro-de-Mateo.local>
2023-06-10 18:57:07 -0300

    songs

--- js/songs.js
+++ js/songs.js
@@ -1,4 +1,4 @@
-let urlsongs = "https://cors-anywhere.herokuapp.com/https://developers.deezer.com/api/chart "
+let urlsongs = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/908622995"
     fetch(urlsongs) // Usamos fetch ya que nos permite consultar un recurso de forma asincronica, es decir, iniciar una accion sin depender de la finalizacion de acciones anteriores.
     .then(function(respuesta){ // En este caso el parametro "(response)" es el que recibe la informacion del endpoint, obtenida por el fetch.
         return respuesta.json(); // Utiilizamos el metodo "json()" con el fin de decodificar la informacion obtenida por el fetch y poder utilizarla en forma de arrays, objetos, etc... Al ser su proceso asincronico, necesitamos de otra promesa, es decir, un segundo "then"
